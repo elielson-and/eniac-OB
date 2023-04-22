67c1e37b7f9f14d5ac6e8ea24bf391c176d5d842
Elielson André <68979955+elielson-andre@users.noreply.github.com>
2023-04-22 12:19:50 -0300

    bugfix: IqOption Auth checker

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from app.Checkers.Checker import Checker
 from database.Connection import ConexaoMySQL
 import curses
 
+
 SplashScreen.show()
 
 # Running checkers before start
 checker = Checker()
-
